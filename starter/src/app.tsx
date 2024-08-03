a8ed2acf6a4cbe97ced3942696887e377e6d11f9
Gürkan Gökmen <122023578+gurkangokmen@users.noreply.github.com>
2024-08-03 12:45:48 +0300

    Initialized map-nearby-search-react project with google-starter-pack.

---
+++ starter/src/app.tsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import { createRoot } from "react-dom/client";
+
+
+const App = () => {
+    
+
+    return (<div></div>);
+};
+
+
+
+const root = createRoot(document.getElementById('app'));
+root.render(<App />);
+
+export default App;
